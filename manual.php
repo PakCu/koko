c0f4e75bbe1261080cc568d81671dfd48e26d9cc
PakCu <bushak@gmail.com>
2012-06-04 01:41:22 +0800

    Baru: Manual Pengguna

---
+++ manual.php
@@ -0,0 +1,8 @@
+<?php
+if (!isset($_SESSION)) { session_start(); }
+include('inc/conn.php'); //connection database
+include('inc/fungsi.php'); //panggil fail fungsi
+include('tmp/header.php'); //panggil fail header
+include('tmp/manual.php'); //panggil fail content
+include('tmp/footer.php'); //panggil fail footer
+?>
---
+++ tmp/manual.php
@@ -0,0 +1,19 @@
+<table width="100%" border="0" cellpadding="2" cellspacing="0">
+  <tr>
+    <td width="100%" align="center" valign="top">
+      <br />
+      <table width="860" border="1" cellspacing="0" cellpadding="5">
+        <tr>
+          <td align="center" bgcolor="#49A3FF" class="tkmenu2">MANUAL PENGGUNAAN SISTEM</td>
+        </tr>
+      </table>
+      <table width="850" border="1" cellspacing="0" cellpadding="3" class="table2">
+        <tr>
+          <td align="center" valign="middle" bgcolor="#FFFFFF">
+          	<embed src="files/manual.pdf#toolbar=0&amp;navpanes=0&amp;scrollbar=auto" width="850" height="500">
+          </td>
+          </tr>
+      </table>
+    <br></td>
+  </tr>
+</table>
